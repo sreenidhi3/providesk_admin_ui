62e4039e4e31ddcbdf0dadaae360c4131c1157d4
Suraj Rane <suraj.rane@druva.local>
2022-09-17 01:55:22 +0530

    Added bootstrap library

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from "./App";
+import App from './App';
 
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "about",
+    path: 'about',
     element: <div>About</div>,
   },
 ]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
