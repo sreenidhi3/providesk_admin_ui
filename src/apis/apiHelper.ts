33d84c8786782ff2166de25534bc4535186544d4
Suraj Rane <suraj.rane@druva.local>
2022-09-16 19:47:27 +0530

    Added api helper files & created axios instance config

---
+++ src/apis/apiHelper.ts
@@ -0,0 +1,39 @@
+import { axiosInstance } from "./apiConfigInitializer";
+import { deletePayload, getPayload, postPayload, putPayload } from "./types";
+
+export const get = async (payload: getPayload) => {
+  const { path, queryParams = null, responseType = "json" } = payload;
+  const response = await axiosInstance.get(path, {
+    params: queryParams,
+    responseType,
+  });
+  return response;
+};
+
+/**
+ * post will be used to as generic helper method to make async n/w post request
+ * @param payload
+ */
+export const post = async (payload: postPayload) => {
+  const { path, requestParams, queryParams = {} } = payload;
+  const response = await axiosInstance.post(path, requestParams, {
+    params: queryParams,
+  });
+  return response;
+};
+
+/**
+ * put will be used to as generic helper method to make async n/w put requests
+ * @param payload
+ */
+export const put = async (payload: putPayload) => {
+  const { path, payloadParams } = payload;
+  const response = await axiosInstance.put(path, payloadParams);
+  return response;
+};
+
+export const deleteCall = async (payload: deletePayload) => {
+  const { path, queryParams = null } = payload;
+  const response = await axiosInstance.delete(path, { params: queryParams });
+  return response;
+};
---
+++ src/apis/types.ts
@@ -0,0 +1,21 @@
+export interface getPayload {
+  path: string;
+  queryParams?: object;
+  responseType?: any; // in case we need change the response type
+}
+
+export interface postPayload {
+  path: string;
+  requestParams?: object;
+  queryParams?: object;
+}
+
+export interface putPayload {
+  path: string;
+  payloadParams?: object;
+}
+
+export interface deletePayload {
+  path: string;
+  queryParams?: object;
+}
