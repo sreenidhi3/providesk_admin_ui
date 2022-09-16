b0f660d5d393b98cc7f0af073f90b3b41c9c5c25
Suraj Rane <suraj.rane@druva.local>
2022-09-16 20:19:33 +0530

    Added prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Welcome to Providesk</h1>
       </header>
     </div>
