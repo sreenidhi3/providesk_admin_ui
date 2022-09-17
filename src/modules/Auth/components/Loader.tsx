a93dca0e7c24030a06e93a5121d6f473d188612e
Sreenidhi Bendre <sreenidhi.bendre@gmail.com>
2022-09-17 14:55:35 +0530

    Removed explicit return statement

--- src/modules/Auth/components/Loader.tsx
+++ src/modules/Auth/components/Loader.tsx
@@ -5,16 +5,14 @@ interface IProps {
   isLoading: boolean;
 }
 
-const Loader: React.FC<IProps> = ({ isLoading }) => {
-  return (
-    <Backdrop
-      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      open={isLoading}
-      onClick={() => {}}
-    >
-      <CircularProgress color='inherit' />
-    </Backdrop>
-  );
-};
+const Loader: React.FC<IProps> = ({ isLoading }) => (
+  <Backdrop
+    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    open={isLoading}
+    onClick={() => {}}
+  >
+    <CircularProgress color='inherit' />
+  </Backdrop>
+);
 
 export default Loader;
