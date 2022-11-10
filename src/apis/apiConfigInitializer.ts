5f126a16ebae099e12dc45816841e310ba6b03c3
Sreenidhi Bendre <sreenidhi.bendre@gmail.com>
2022-11-10 10:56:45 +0530

    Add: axios request interceptor to set authorization header and response interceptor to check for invalid token

--- src/apis/apiConfigInitializer.ts
+++ src/apis/apiConfigInitializer.ts
@@ -1,6 +1,10 @@
-import axios from 'axios';
+import axios, { AxiosRequestHeaders } from 'axios';
 
 import { API_BASE_URL } from 'shared/appConstants';
+import {
+  loadLocalStorage,
+  removeLocalStorageState,
+} from 'shared/localStorageHelpers';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -9,3 +13,27 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// adds token to requests
+axiosInstance.interceptors.request.use((config) => {
+  let headers = config.headers as AxiosRequestHeaders;
+  let token = loadLocalStorage('userAuth');
+  if (token) {
+    headers['Authorization'] = token.auth_token;
+  }
+  return config;
+});
+
+// redirects user to login if user is unauthorized or if the token has expired
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error);
+    if (error.response && error.response.data) {
+      window.location.href = '/';
+      removeLocalStorageState('userAuth');
+      removeLocalStorageState('userProfile');
+    }
+    return;
+  }
+);
