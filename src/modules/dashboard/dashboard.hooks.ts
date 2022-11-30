ee9828679dccf9a5e616066fb934c402d9e2134c
Sreenidhi Bendre <sreenidhi.bendre@gmail.com>
2022-11-30 17:43:33 +0530

    Fix: timeline and dashboard UI changes

--- src/modules/dashboard/dashboard.hooks.ts
+++ src/modules/dashboard/dashboard.hooks.ts
@@ -5,24 +5,27 @@ import { getRequestList } from './dashboard.services';
 
 export const useGetRequestsList = (queryParams: IFetchComplaintListRequest) => {
   let path = '/tickets';
-  
-   let params = {
-    type : queryParams?.type!=="" ? queryParams?.type:undefined,
-    category:queryParams?.category!=="" ? queryParams?.category:undefined ,
-    status : queryParams?.status!=="" ? queryParams.status:undefined,
-    department : queryParams?.department!=="" ? queryParams.department:undefined,
-    assigned_to_me:queryParams?.assig_to_me=== true ? true:undefined,
-    created_by_me:queryParams?.created_by_me===true ? true:undefined,
-  }
-  const { data, isLoading, isFetching } = useQuery(['complaint-list',params], () =>
-    getRequestList(path,params),{
-      onError:(e)=>{
-        toast.error("Failed to fetch list")
-      }
+
+  let params = {
+    type: queryParams?.type !== '' ? queryParams?.type : undefined,
+    category: queryParams?.category !== '' ? queryParams?.category : undefined,
+    status: queryParams?.status !== '' ? queryParams.status : undefined,
+    department:
+      queryParams?.department !== '' ? queryParams.department : undefined,
+    assigned_to_me: queryParams?.assig_to_me === true ? true : undefined,
+    created_by_me: queryParams?.created_by_me === true ? true : undefined,
+  };
+  const { data, isLoading, isFetching } = useQuery(
+    ['complaint-list', params],
+    () => getRequestList(path, params),
+    {
+      onError: (e) => {
+        toast.error('Failed to fetch list');
+      },
     }
   );
   return {
-    data: data?.data,
+    data: data?.data?.data,
     isLoading: isLoading || isFetching,
   };
 };
--- src/modules/details/components/Timeline.tsx
+++ src/modules/details/components/Timeline.tsx
@@ -10,6 +10,7 @@ import {
 import { Box, Chip, Paper, Typography } from '@mui/material';
 import { DateFormate, getLastDaysFrom } from 'apis/utils/date.utils';
 import { ticketStatusColours } from '../constants';
+import { ITicketActivity } from '../type';
 
 export const TimelineComponent = ({ activities }: any) => {
   return (
@@ -54,7 +55,7 @@ const TimelineLeft = ({ date, index }: { date: string; index: number }) => {
   );
 };
 
-const TimeLineDescription = ({ activity }: { activity: any }) => {
+const TimeLineDescription = ({ activity }: { activity: ITicketActivity }) => {
   return (
     <Paper elevation={8} style={{ width: '40vw', maxWidth: '100%' }}>
       <div style={{ padding: '0.5rem 0', margin: '0.5rem' }}>
@@ -66,12 +67,13 @@ const TimeLineDescription = ({ activity }: { activity: any }) => {
           }}
           sx={{ m: 2 }}
         />
-        <p>{activity?.description}</p>
-        <p>
-          {activity?.reason_for_update && (
-            <strong>Reason: {activity?.reason_for_update}</strong>
-          )}
-        </p>
+        <span>{activity?.get_description}</span>
+        {activity.reason_for_update && (
+          <p>
+            <strong>Comment:</strong>
+            <span> {activity?.reason_for_update}</span>
+          </p>
+        )}
       </div>
     </Paper>
   );
